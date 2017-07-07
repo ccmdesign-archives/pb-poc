520aeb33db68e97a08d07ddd425582305fe11cd7
Claudio Mendonca <claudioccm@gmail.com>
2017-07-07 11:54:44 -0400

    base structure

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    $('.js-trigger').click(function () {
+      $(this).parent().toggleClass('js-active');
+    });
+
+
+
+
     // Smooth Scrolling Function
     $('a[href*=#]:not([href=#])').click(function () {
         var $targ = $(this.hash),
@@ -18,59 +25,5 @@ $(document).ready(function () {
         }
 
         return true;
-    });
-
-    // Modal Click Behavior
-    $('.js-open-modal').click(function () {
-        $('.js-target-modal').addClass('js-active');
-        $('#overlay').addClass('js-active');
-        $('body').addClass('js-body-modal-active');
-    });
-
-    $('.js-close-modal').click(function () {
-        $('.js-target-modal').removeClass('js-active');
-        $('#overlay').removeClass('js-active');
-        $('body').removeClass('js-body-modal-active');
-    });
-
-    // Sticky Click Behavior
-    $('.js-close-sticky').click(function () {
-        $('.js-target-sticky').removeClass('js-active');
-    });
-
-    // Search Click Behavior
-    $('.js-trigger-search').click(function (e) {
-        e.preventDefault();
-        $(this).parent().addClass('js-active');
-        $('#overlay').addClass('js-active');
-    });
-
-    // Main Menu Click Behavior
-    $('.js-trigger-menu').click(function (e) {
-        $(this).next().addClass('js-active-menu');
-        $('#overlay').addClass('js-active');
-    });
-
-    // General Click Behavior for Overlay
-    $('#overlay').click(function () {
-        $('.js-active').removeClass('js-active');
-        $('.js-active-menu').removeClass('js-active-menu');
-    });
-
-    // Slider
-    $('.slider').slick({
-        arrows: true,
-        draggable: false,
-        swipeToSlide: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-            {
-                breakpoint: 800,
-                settings: {
-                    draggable: true
-                }
-            }
-        ]
     });
 }); // doc.ready
